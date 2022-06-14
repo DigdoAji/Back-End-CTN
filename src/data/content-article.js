98a83dd3277a0f86c747c36e51b26a25601ed3fc
DigdoAji <1910817310001@mhs.ulm.ac.id>
2022-06-14 20:47:42 +0800

    Update data content article/event dan tambah object timezone pada content event

--- src/data/content-article.js
+++ src/data/content-article.js
@@ -5,7 +5,7 @@ const contentArticles = [
         description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet.',
         pictureId: 'https://i.postimg.cc/0j4FpcN7/homestay-di-pulau-samosir.jpg',
         publisherName: 'Alexa',
-        publishDate: '20 May 2022',
+        publishDate: '2022-05-20',
         categories: 'Tourism',
         userReviews: [
           {
@@ -28,7 +28,7 @@ const contentArticles = [
         description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet.',
         pictureId: 'https://i.postimg.cc/6q8CGhgs/fasilitas-di-batu-seribu.webp',
         publisherName: 'Karin',
-        publishDate: '25 May 2022',
+        publishDate: '2022-05-25',
         categories: 'Tourism',
         userReviews: [
           {
@@ -45,7 +45,7 @@ const contentArticles = [
         description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet.',
         pictureId: 'https://i.postimg.cc/d1ByffPc/mountain-hero-1.jpg',
         publisherName: 'Adam',
-        publishDate: '30 May 2022',
+        publishDate: '2022-05-30',
         categories: 'Tourism',
         userReviews: [
           {
@@ -68,7 +68,7 @@ const contentArticles = [
         description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet.',
         pictureId: 'https://i.postimg.cc/qvzWvfjQ/gigiruncingjpg-629973babb44865cea37b2a4.jpg',
         publisherName: 'Putra',
-        publishDate: '3 June 2022',
+        publishDate: '2022-06-03',
         categories: 'Culture',
         userReviews: [
           {
--- src/data/content-events.js
+++ src/data/content-events.js
@@ -3,11 +3,12 @@ const contentEvents = [
         id: 'w245tke54nsgvbku',
         name: 'Rinjani Begawe Festival',
         location: 'Senaru, West Nusa Tenggara',
-        date: '10 March 2022',
-        time: '10.00 WITA - 20.00 WITA',
+        date: '2022-03-10',
+        time: '10:00',
+        timezone: 'WITA',
         description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet.',
         pictureId: 'https://i.postimg.cc/x82zpSFR/rinjani-begawe-festival.jpg',
-        categories: 'Culture, Festival',
+        categories: 'Festival',
         userReviews: [
           {
               reviewId: 'wcuq4z',
@@ -21,11 +22,12 @@ const contentEvents = [
         id: 'cn6oeawy7rtftrcq',
         name: "World Dance Day 2020 Toraya Ma'Gellu",
         location: 'Buntu Pune, South Celebes',
-        date: '22 April 2022 - 23 April 2022',
-        time: '08.00 WITA - 16.00 WITA',
+        date: '2022-04-26',
+        time: '08:00',
+        timezone: 'WITA',
         description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet.',
         pictureId: 'https://i.postimg.cc/25GyjD13/hari-tari-dunia-toraja.jpg',
-        categories: 'Tourism, Culture',
+        categories: 'Culture Show',
         userReviews: [
           {
               reviewId: '2aitr0',
@@ -45,11 +47,12 @@ const contentEvents = [
         id: 'ea13o4yd4kxc4ioo',
         name: 'Banjar Traditional Rowboat Competition',
         location: 'Martapura, South Borneo',
-        date: '15 May 2022',
-        time: '08.00 WITA - 12.00 WITA',
+        date: '2022-05-15',
+        time: '08:00',
+        timezone: 'WITA',
         description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet.',
         pictureId: 'https://i.postimg.cc/QMKKXM2m/lomba-jukung-tradisonal-banjar.png',
-        categories: 'Tourism, Culture, Competition',
+        categories: 'Competition',
         userReviews: [
           {
               reviewId: 'bemx6n',
@@ -69,11 +72,12 @@ const contentEvents = [
         id: '6pgb0ldp1dofiijr',
         name: 'Saijan Culture Festival 2022',
         location: 'Kotabaru, South Borneo',
-        date: '04 June 2022',
-        time: '09.00 WITA - 16.00 WITA',
+        date: '2022-06-04',
+        time: '09:00',
+        timezone: 'WITA',
         description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet.',
         pictureId: 'https://i.postimg.cc/mgRmzvL7/festival-budaya-saijan-2022.jpg',
-        categories: 'Culture, Festival',
+        categories: 'Festival',
         userReviews: [
           {
               reviewId: 'zvbv8w',
